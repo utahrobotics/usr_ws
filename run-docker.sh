9c1d05155e7a886a264627d06ff8a888489a15d2
Carson Storm <stormymcstorm@gmail.com>
2018-11-13 21:45:17 -0700

    add X11 forwardin to run-docker.sh and better documentation for script

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-docker run -it -v $PWD:/home/utahrobotics/usr_ws usr_ws
+# this script is for running the usr_ws docker continer on linux machines
+# to build to image do `docker build -t usr_ws .`
+
+docker run -it \
+    -e "DISPLAY" \
+    -v $PWD:/home/utahrobotics/usr_ws \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    usr_ws
